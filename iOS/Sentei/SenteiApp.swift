02f11fc9244f06868d41d482110b46fb32b7dc13
Valerii <complexityclass@gmail.com>
2024-03-24 20:47:49 +0000

    Add iOS project

---
+++ iOS/Sentei/SenteiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SenteiApp.swift
+//  Sentei
+//
+//  Created by Valerii Popov on 24/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(core: Core())
+        }
+    }
+}
---
+++ iOS/Sentei/core.swift
@@ -0,0 +1,34 @@
+//
+//  core.swift
+//  Sentei
+//
+//  Created by Valerii Popov on 24/03/2024.
+//
+
+import Foundation
+import SharedTypes
+
+@MainActor
+class Core: ObservableObject {
+    @Published var view: ViewModel
+    
+    init() {
+        self.view = try! .bincodeDeserialize(input: [UInt8](Sentei.view()))
+    }
+    
+    func update(_ event: Event) {
+        let effects = [UInt8](processEvent(Data(try! event.bincodeSerialize())))
+        
+        let requests: [Request] = try! .bincodeDeserialize(input: effects)
+        for request in requests {
+            processEffect(request)
+        }
+    }
+    
+    func processEffect(_ request: Request) {
+        switch request.effect {
+        case .render:
+            view = try! .bincodeDeserialize(input: [UInt8](Sentei.view()))
+        }
+    }
+}
