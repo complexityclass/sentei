f8db9e76a758290c11293c7a1e3e8042cd0c0787
Valerii <complexityclass@gmail.com>
2024-04-01 19:01:49 +0100

    Add tests for the board implementation

--- go_bot/src/board.rs
+++ go_bot/src/board.rs
@@ -68,8 +68,23 @@ impl Board {
 
         for other_color_string in &adj_opposite_color {
             if other_color_string.liberties_count() == 0 {
-                todo!("remove string")
+                self.remove_string(other_color_string);
             }
+        }
+    }
+
+    fn remove_string(&mut self, string: &StonesString) {
+        for point in &string.stones {
+            for neighbor in point.neighbors() {
+                if let Some(neighbor_string) = self.grid.get_mut(&neighbor) {
+                    if neighbor_string != string {
+                        neighbor_string.add_liberty(&point);
+                    }
+                } else {
+                    continue;
+                }
+            }
+            self.grid.remove(&point);
         }
     }
 
@@ -94,11 +109,127 @@ impl Board {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::player::Player::{Black, White};
 
     #[test]
     #[should_panic(expected = "Item out of the board size")]
     fn test_throws_exception_if_out_of_grid() {
         let mut board = Board::new(9, 9);
-        board.place_stone(Player::White, Point::new(10, 10));
+        board.place_stone(White, Point::new(10, 10));
+    }
+
+    #[test]
+    #[should_panic(expected = "Item out of the board size")]
+    fn test_throws_exception_if_out_of_grid2() {
+        let mut board = Board::new(9, 9);
+        board.place_stone(White, Point::new(0, 1));
+    }
+
+    #[test]
+    fn test_place_stone() {
+        let mut board = Board::new(4, 4);
+        board.place_stone(White, Point::new(1, 1));
+        for i in 0..4 {
+            for j in 0..4 {
+                let p = Point::new(i, j);
+                if i == 1 && j == 1 {
+                    assert_eq!(board.get(p), Some(White));
+                } else {
+                    assert!(board.get(p).is_none());
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_place_stones_in_string() {
+        let mut board = Board::new(4, 4);
+        let black_stones = vec![
+            Point::new(1, 1),
+            Point::new(1, 2),
+            Point::new(1, 3),
+            Point::new(2, 1),
+        ];
+        let white_stones = vec![Point::new(4, 2), Point::new(4, 3), Point::new(4, 4)];
+
+        // Place Black stones
+        for stone in &black_stones {
+            board.place_stone(Black, *stone);
+        }
+
+        // Place White stones
+        for stone in &white_stones {
+            board.place_stone(White, *stone);
+        }
+
+        // Check Black string
+        let black_string = board.get_string(Point::new(1, 1)).unwrap();
+        for stone in &black_stones {
+            assert_eq!(
+                black_string,
+                board.get_string(*stone).unwrap(),
+                "Black stone at {:?} should be part of the same string",
+                stone
+            );
+        }
+
+        // Check White string
+        let white_string = board.get_string(Point::new(4, 2)).unwrap();
+        for stone in &white_stones {
+            assert_eq!(
+                white_string,
+                board.get_string(*stone).unwrap(),
+                "White stone at {:?} should be part of the same string",
+                stone
+            );
+        }
+
+        // Check for no stones at other points
+        for i in 1..=4 {
+            for j in 1..=4 {
+                let p = Point::new(i, j);
+                if !black_stones.contains(&p) && !white_stones.contains(&p) {
+                    assert!(
+                        board.get_string(p).is_none(),
+                        "Point {:?} should not belong to any string",
+                        p
+                    );
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_capture() {
+        let mut board = Board::new(4, 4);
+        // Put white stone in the middle surrounded by 3 black stones
+        let wp = Point::new(2, 2);
+        board.place_stone(White, wp);
+
+        let black_stones = vec![Point::new(1, 2), Point::new(2, 1), Point::new(2, 3)];
+
+        for stone in &black_stones {
+            board.place_stone(Black, *stone);
+        }
+
+        assert_eq!(board.get(wp), Some(White));
+        assert_eq!(board.get(Point::new(2, 1)), Some(Black));
+        assert_eq!(
+            board.get_string(Point::new(2, 1)).unwrap().liberties.len(),
+            2
+        );
+
+        // Add 4th black stone
+        board.place_stone(Black, Point::new(3, 2));
+
+        // White stone captured
+        todo!("Fix this test!");
+        assert!(board.get(wp).is_none());
+
+        // Black has one more liberty
+        assert_eq!(
+            board.get_string(Point::new(2, 1)).unwrap().liberties.len(),
+            3
+        );
     }
 }
